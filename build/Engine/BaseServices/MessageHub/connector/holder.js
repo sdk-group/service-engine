c0e25f893a23a7718002c126bc7d232b9016a876
Oleg Abrosimov <olegabr@yandex.ru>
2016-01-05 12:19:18 +0600

    сборка после одного из последних pull-ов

--- build/Engine/BaseServices/MessageHub/connector/holder.js
+++ build/Engine/BaseServices/MessageHub/connector/holder.js
@@ -1,95 +1,91 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-	value: true
-});
 let AbstractConnector = require("./abstract");
 
-let get_model = function (name) {
-	if (_.isString(name)) {
-		try {
-			return require(`./${ name }`);
-		} catch (e) {
-			return undefined;
-		}
-	}
-	return undefined;
+let getModel = function (name) {
+  if (_.isString(name)) {
+    try {
+      return require(`./${ name }`);
+    } catch (e) {
+      return undefined;
+    }
+  }
+  return undefined;
 };
 
 class ConnectorHolder {
-	constructor() {
-		let default_options = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+  constructor() {
+    let default_options = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
 
-		this.default_options = default_options;
-		this.connectors = {};
-	}
+    this.default_options = default_options;
+    this.connectors = {};
+  }
 
-	addMulti(conn_map) {
-		return _.mapValues(conn_map, (conn_data, conn_key) => {
-			return this.add({
-				model: conn_data.model,
-				key: conn_data.key || conn_key
-			}, conn_data.options || this.default_options);
-		});
-	}
+  addMulti(conn_map) {
+    return _.mapValues(conn_map, (conn_data, conn_key) => {
+      return this.add({
+        model: conn_data.model,
+        key: conn_data.key || conn_key
+      }, conn_data.options || this.default_options);
+    });
+  }
 
-	add(connector, options) {
-		try {
-			let Model = get_model(connector.model);
-			if (_.isUndefined(Model)) return false;
-			let n_connector = new Model();
-			n_connector.create(options || this.default_options[connector.model]);
-			this.connectors[connector.key] = n_connector;
-			return true;
-		} catch (e) {
-			return false;
-		}
-	}
+  add(connector, options) {
+    try {
+      let Model = getModel(connector.model);
+      if (_.isUndefined(Model)) return false;
+      let n_connector = new Model();
+      n_connector.create(options || this.default_options[connector.model]);
+      this.connectors[connector.key] = n_connector;
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
 
-	remove(conn_key) {
-		try {
-			if (this.connectors[conn_key]) {
-				this.connectors[conn_key].close();
-				delete this.connectors[conn_key];
-			}
-		} catch (e) {
-			return false;
-		}
-		return true;
-	}
+  remove(conn_key) {
+    try {
+      if (this.connectors[conn_key]) {
+        this.connectors[conn_key].close();
+        delete this.connectors[conn_key];
+      }
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
 
-	connector(conn_key) {
-		return this.connectors[conn_key] || false;
-	}
+  connector(conn_key) {
+    return this.connectors[conn_key] || false;
+  }
 
-	listen() {
-		return _.map(this.connectors, conn => {
-			return conn.listen();
-		});
-	}
+  listen() {
+    return _.map(this.connectors, conn => {
+      return conn.listen();
+    });
+  }
 
-	close() {
-		return _.map(this.connectors, conn => {
-			return conn.close();
-		});
-	}
+  close() {
+    return _.map(this.connectors, conn => {
+      return conn.close();
+    });
+  }
 
-	on_message(resolver) {
-		_.map(this.connectors, conn => {
-			return conn.on_message(data => {
-				return resolver(data);
-			});
-		});
-	}
+  on_message(resolver) {
+    _.map(this.connectors, conn => {
+      return conn.on_message(data => {
+        return resolver(data);
+      });
+    });
+  }
 
-	on_login(resolver) {
-		_.map(this.connectors, conn => {
-			return conn.on_login(data => {
-				return resolver(data);
-			});
-		});
-	}
+  on_login(resolver) {
+    _.map(this.connectors, conn => {
+      return conn.on_login(data => {
+        return resolver(data);
+      });
+    });
+  }
 }
 
-exports.default = ConnectorHolder;
-module.exports = exports.default;
+module.exports = ConnectorHolder;
