36bfd85e60230c7e202ddf5b1181e0bdf22cc301
YuuKohaku <soul.of.bytes@gmail.com>
2016-03-07 04:35:19 +0600

    1

--- src/config/Constellation/constellation.js
+++ src/config/Constellation/constellation.js
@@ -10,44 +10,45 @@ let _ = require('lodash');
 let Stella = require("./stella");
 
 class Constellation {
-    constructor() {
-        this.list = {};
-        this.forEach = _.partial(_.forEach, this.list);
-    }
+	constructor() {
+		this.list = {};
+		this.forEach = _.partial(_.forEach, this.list);
+	}
 
-    show(hostname) {
-        return this.list[hostname] || this.show_by_IP(hostname);
-    }
+	show(hostname) {
+		return this.list[hostname] || this.show_by_IP(hostname);
+	}
 
-    show_by_IP(ip) {
-        let stella = _.find(this.list, {
-            "ip": ip
-        });
-        return stella;
-    }
+	show_by_IP(ip) {
+		let stella = _.find(this.list, {
+			"ip": ip
+		});
+		return stella;
+	}
 
-    add(hostname, ip, credentials) {
-        let stella = new Stella(ip, hostname, credentials);
-        this.list[hostname] = stella;
-        return this;
-    }
+	add(hostname, ip, credentials) {
+		let stella = new Stella(ip, hostname, credentials);
+		this.list[hostname] = stella;
+		return this;
+	}
 
-    remove(hostname) {
-        delete this.list[hostname];
-        return this;
-    }
+	remove(hostname) {
+		_.unset(this.list, hostname);
+		return this;
+	}
 
-    update_credentials(hostname, ip, credentials) {
-        this.remove(hostname).add(hostname, ip, credentials);
-        return this;
-    }
+	update_credentials(hostname, ip, credentials) {
+		this.remove(hostname)
+			.add(hostname, ip, credentials);
+		return this;
+	}
 
-    lapse(ip, on = false) {
-        let stella = _.find(this.list, {
-            "ip": ip
-        });
-        stella.active = !!on;
-    }
+	lapse(ip, on = false) {
+		let stella = _.find(this.list, {
+			"ip": ip
+		});
+		stella.active = !!on;
+	}
 }
 
 module.exports = Constellation;
--- src/config/loader.js
+++ src/config/loader.js
@@ -1,92 +1,78 @@
 "use strict";
-let _ = require("lodash");
-let Promise = require("bluebird");
-let traverse = require("traverse");
 let couchbird = require("Couchbird")();
 
-let Config = function(bucket) {
+let Config = function (bucket) {
 	let _cfg_keys = {};
 	let _cfg_origs = {};
 
 	let _db = couchbird.bucket(bucket);
-	let _path_delimiter = ".";
 
-	let _promisedConfig = function(cfg_id, cfg_name) {
-		return _db.get(cfg_id).then(function(res) {
-				if(!_.has(_cfg_keys, cfg_name))
+	let _promisedConfig = function (cfg_id, cfg_name) {
+		return _db.get(cfg_id)
+			.then((res) => {
+				if (!_.has(_cfg_keys, cfg_name))
 					_cfg_keys[cfg_name] = cfg_id;
 				_cfg_origs[cfg_name] = res.value;
 				let def = _.cloneDeep(res.value.default);
 				let result = (!def || _.isEmpty(def)) ? res.value : _.merge(def, res.value);
 				return Promise.resolve(result);
 			})
-			.catch(function(err) {
+			.catch((err) => {
 				_cfg_keys = _.omit(_cfg_keys, cfg_name);
 				return Promise.resolve(false);
 			});
 	}
 
-	let _getTraverse = function(obj, where) {
-		let path = _.isArray(where) ? where : where.split(_path_delimiter);
-		path = (_.head(path) != "default") ? path : _.tail(path);
-		return traverse(obj).get(path);
-	}
-
 	let pub = {
-		set_path_delimiter: function(delim) {
-			_path_delimiter = delim;
-		},
-		clean: function() {
-			let self = this;
-			_.forEach(_cfg_keys, function(val, key) {
-				delete self[key];
-				delete _cfg_origs[key];
+		clean: function () {
+			_.forEach(_cfg_keys, (val, key) => {
+				_.unset(this, key);
+				_.unset(_cfg_origs, key);
 			});
 		},
-		load: function(cfg_ids) {
-			if(!_.isObject(cfg_ids))
+		load: function (cfg_ids) {
+			if (!_.isObject(cfg_ids))
 				throw new Error("INVALID_ARGUMENT: A list of config ids should be passed");
 			let props = {};
-			let self = this;
 			_.forEach(cfg_ids,
-				function(val, key) {
+				(val, key) => {
 					props[key] = _promisedConfig(val, key);
 				});
 			return Promise.props(props)
-				.then(function(res) {
-					_.forEach(_cfg_keys, function(val, key) {
-						self[key] = res[key];
+				.then((res) => {
+					_.forEach(_cfg_keys, (val, key) => {
+						this[key] = res[key];
 					});
-					return Promise.resolve(self);
+					return Promise.resolve(this);
 				});
 		},
-		init: function() {
+		init: function () {
 			return this.load();
 		},
-		reload: function() {
+		reload: function () {
 			this.clean();
 			return this.load(_cfg_keys);
 		},
-		get_default: function(cfg, path) {
-			if(!_.isArray(path) && !_.isString(path))
-				throw new Error("INVALID_ARGUMENT: Path should be either string with '" + _path_delimiter + "' as delimiter, or an array.");
-			if(!_.has(_cfg_keys, cfg) || !_.has(_cfg_origs, cfg + ".default"))
+		get_default: function (cfg, path) {
+			if (!_.isArray(path) && !_.isString(path))
+				throw new Error("INVALID_ARGUMENT: Path must be either string, or an array.");
+			if (!_.has(_cfg_keys, cfg) || !_.has(_cfg_origs, cfg + ".default"))
 				return {};
-			return _getTraverse(_cfg_origs[cfg].default, path);
+			return _.get(_cfg_origs[cfg].default, path);
 		},
-		get_nodefault: function(cfg, path) {
-			if(!_.isArray(path) && !_.isString(path))
-				throw new Error("INVALID_ARGUMENT: Path should be either string with '" + _path_delimiter + "' as delimiter, or an array.");
-			if(!_.has(_cfg_keys, cfg))
+		get_nodefault: function (cfg, path) {
+			if (!_.isArray(path) && !_.isString(path))
+				throw new Error("INVALID_ARGUMENT: Path must be either string, or an array.");
+			if (!_.has(_cfg_keys, cfg))
 				return {};
-			return _getTraverse(_cfg_origs[cfg], path);
+			return _.get(_cfg_origs[cfg], path);
 		},
-		safe_get: function(cfg, path) {
-			if(!_.isArray(path) && !_.isString(path))
-				throw new Error("INVALID_ARGUMENT: Path should be either string with '" + _path_delimiter + "' as delimiter, or an array.");
-			return _getTraverse(_cfg_origs[cfg], path) || _getTraverse(_cfg_origs[cfg].default, path);
+		safe_get: function (cfg, path) {
+			if (!_.isArray(path) && !_.isString(path))
+				throw new Error("INVALID_ARGUMENT: Path must be either string, or an array.");
+			return _.get(_cfg_origs[cfg], path) || _.get(_cfg_origs[cfg].default, path);
 		},
-		get: function(cfg, path) {
+		get: function (cfg, path) {
 			return this.safe_get(cfg, path);
 		}
 	};
