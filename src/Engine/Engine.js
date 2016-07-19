cb93736e8a0a7a4682a960671648459ad112ffb7
YuuKohaku <soul.of.bytes@gmail.com>
2016-07-20 04:05:58 +0600

    Sequentive launch

--- src/Engine/Engine.js
+++ src/Engine/Engine.js
@@ -4,58 +4,62 @@ let AbstractService = require('./BaseServices/Abstract');
 let Servicify = require('./BaseServices/Servicify');
 let Loader = require('../config/loader');
 
-let discover = function(path_string) {
-  return require(path_string);
+let discover = function (path_string) {
+	return require(path_string);
 };
 
 class Engine {
-  constructor() {
-    throw new Error('singletone');
-  }
-  static set config(value) {
-    let cfg_ready = false;
-    if (value.config_key && value.buckets.config) {
-      let loader = Loader(value.buckets.config);
-      cfg_ready = loader.load({
-          services: value.config_key
-        })
-        .then(() => {
-          return loader.services;
-        });
-    } else {
-      cfg_ready = Promise.resolve(value);
-    }
-    this.cfg_ready = cfg_ready
-      .then((config) => {
-        let main_group = config.main_group;
-        this.service_params = _.map(main_group, 'params');
-        this.services = _.map(main_group, (item) => this.createService(item.path));
-        return true;
-      })
-      .catch((err) => {
-        console.error(err.stack);
-        return false;
-      });
-    this.cfg = value;
-  }
-  static createService(path) {
-    let ServiceModel = discover(path);
-    return (ServiceModel.constructor.name != "Object") ? new ServiceModel() : new Servicify(ServiceModel)
-  }
-  static launch() {
-    return this.cfg_ready
-      .then((res) => {
-        let init = _.fromPairs(_.map(this.services, (service, index) => [service.getName(), service.init(this.service_params[index], this.cfg)]));
-        return Promise.props(init);
-      })
-      .then(() => Promise.props(_.fromPairs(_.map(this.services, (service) => [service.getName(), service.launch()]))))
-      .then((r) => {
-        message_bus.emit('engine.ready', {
-          status: r
-        });
-        return r;
-      });
-  }
+	constructor() {
+		throw new Error('singletone');
+	}
+	static set config(value) {
+		let cfg_ready = false;
+		if (value.config_key && value.buckets.config) {
+			let loader = Loader(value.buckets.config);
+			cfg_ready = loader.load({
+					services: value.config_key
+				})
+				.then(() => {
+					return loader.services;
+				});
+		} else {
+			cfg_ready = Promise.resolve(value);
+		}
+		this.cfg_ready = cfg_ready
+			.then((config) => {
+				let main_group = config.main_group;
+				this.service_params = _.map(main_group, 'params');
+				this.services = _.map(main_group, (item) => this.createService(item.path));
+				return true;
+			})
+			.catch((err) => {
+				console.error(err.stack);
+				return false;
+			});
+		this.cfg = value;
+	}
+	static createService(path) {
+		let ServiceModel = discover(path);
+		return (ServiceModel.constructor.name != "Object") ? new ServiceModel() : new Servicify(ServiceModel)
+	}
+	static launch() {
+		return this.cfg_ready
+			.then((res) => {
+				let init = _.fromPairs(_.map(this.services, (service, index) => [service.getName(), service.init(this.service_params[index], this.cfg)]));
+				return Promise.props(init);
+			})
+			.then(() => Promise.mapSeries(this.services, (service) => service.launch()))
+			.then((res) => _.reduce(this.services, (acc, srv, index) => {
+				acc[srv.getName()] = res[index];
+				return acc;
+			}))
+			.then((r) => {
+				message_bus.emit('engine.ready', {
+					status: r
+				});
+				return r;
+			});
+	}
 }
 
 module.exports = Engine;
